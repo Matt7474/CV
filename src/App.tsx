3b4fc88830243966b905b42ff5c002f4acf410b4
Matt7474 <dimier.matt@gmail.com>
2025-03-24 17:48:39 +0100

    add helmet for CSP

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
             default-src 'self'; 
             script-src 'self' 'unsafe-inline' https://apis.google.com; 
             style-src 'self' 'unsafe-inline'; 
-            img-src 'self' https://images.example.com; 
+            img-src 'self' https://apicv.matt-dev.fr; 
             connect-src 'self' https://apicv.matt-dev.fr; 
             font-src 'self'; 
             object-src 'none'; 
