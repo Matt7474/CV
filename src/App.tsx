71b95ea81cbc7c7c86e96989e64ef347b5dcd532
Matt7474 <dimier.matt@gmail.com>
2025-03-24 19:11:49 +0100

    add helmet for CSP

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,18 @@ function App() {
 			<Helmet>
 				<meta
 					httpEquiv="Content-Security-Policy"
-					content="default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src 'self' https://apicv.matt-dev.fr; ..."
+					content="
+            default-src 'self'; 
+            script-src 'self' 'unsafe-inline' https://apis.google.com; 
+            style-src 'self' 'unsafe-inline'; 
+            img-src 'self' https://apicv.matt-dev.fr; 
+            connect-src 'self' https://apicv.matt-dev.fr; 
+            font-src 'self'; 
+            object-src 'none'; 
+            frame-ancestors 'none'; 
+            base-uri 'self'; 
+            form-action 'self';
+          "
 				/>
 			</Helmet>
 			<BrowserRouter>
